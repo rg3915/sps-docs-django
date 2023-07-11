26212a52a4a546412d2ff748cfd44ad19eb17fda
rg3915 <regis42santos@gmail.com>
2023-07-10 22:39:15 -0300

    Cria o models de Element. close #2

--- backend/spstaglib/admin.py
+++ backend/spstaglib/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SPSElement
+
+
+@admin.register(SPSElement)
+class SPSElementAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'title', 'description')
+    search_fields = ('name', 'title', 'description')
--- backend/spstaglib/models.py
+++ backend/spstaglib/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class SPSElement(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    title = models.CharField(max_length=100, null=True, blank=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'SPSElement'
+        verbose_name_plural = 'SPSElements'
+
+    def __str__(self):
+        return f'{self.name}'
